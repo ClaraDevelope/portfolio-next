c26324c5fc116d1f20c38747e7b858251f645a85
Clara-Manzano <clara.manzano.corona@gmail.com>
2025-07-25 13:51:23 +0200

    post and get fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,35 @@ import TechnologiesSection from "@/components/technologies/TechnologiesSection";
 import { useEffect } from "react";
 
 export default function Page() {
- useEffect(() => {
+  useEffect(() => {
     const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000';
 
-    const source =
-      typeof document !== 'undefined' && document.referrer
-        ? new URL(document.referrer).hostname
-        : 'direct';
+    if (typeof window !== 'undefined') {
+      const urlParams = new URLSearchParams(window.location.search);
+      const sourceParam = urlParams.get('source');
 
-    fetch(`${backendUrl}/api?source=${source}`, {
-      method: 'POST',
-    })
-      .then(res => res.json())
-      .then(data => console.log('Visita registrada:', data))
-      .catch(err => console.error('Error registrando visita:', err));
+      const referrer = document.referrer;
+      const hostnameFromReferrer = referrer ? new URL(referrer).hostname : null;
+
+      const source = sourceParam || hostnameFromReferrer || 'direct';
+
+      fetch(`${backendUrl}/api?source=${source}`, {
+        method: 'POST',
+      })
+        .then(res => res.json())
+        .then(data => console.log('Visita registrada:', data))
+        .catch(err => console.error('Error registrando visita:', err));
+    }
   }, []);
 
   return (
     <div>
-      <Hero/>
-      <ProjectsSection/>
-      <CertificationsSection/>
-      <TechnologiesSection/>
-      <AboutMe/>
-      <ContactSection/>
+      <Hero />
+      <ProjectsSection />
+      <CertificationsSection />
+      <TechnologiesSection />
+      <AboutMe />
+      <ContactSection />
     </div>
   );
 }
