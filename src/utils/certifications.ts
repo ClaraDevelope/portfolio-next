b4168b0669e14a72cf3c5356ce48e124e728b243
Clara-Manzano <clara.manzano.corona@gmail.com>
2025-07-27 13:13:04 +0200

    reorder certifications

--- src/utils/certifications.ts
+++ src/utils/certifications.ts
@@ -1,9 +1,37 @@
 export const CERTIFICATIONS = [
-  {name: "Curso SQL", img: "/images/certificado_SQL.webp", url: "https://tutorial-sql.s3.amazonaws.com/certificates/39285_Clara_Manzano/39285_14112024.png"},
-  {name: "Javascript Developer", img:'/images/Certificado-Javascript.webp', url:"https://verified.sertifier.com/en/verify/03621406770418/"},
-  {name: "Backend Developer", img:'/images/Certificado_Backend.webp', url: "https://verified.sertifier.com/en/verify/99106006683514/"},
-  {name: "Frontend Developer", img:'/images/Certificado_Frontend.webp', url:"https://verified.sertifier.com/es/verify/67005007706277/"},
-  {name: "Certificado MERN FullStack Developer", img: "/images/Certificado_FullStack_MERN.webp", url: "https://verified.sertifier.com/es/verify/56660557762984/"},
-  {name:"Certificado de excelencia académica", img:"/images/certificado_excelencia.webp", url:"https://verified.sertifier.com/es/verify/22221894159946"},
-  {name: "Introducción a la Ingeniería de Software", img: "/images/UAM.webp", url: "https://courses.edx.org/certificates/9e839c780889498db42a5d340f59aada"},
-]
+  {
+    name: "Certificado MERN FullStack Developer",
+    img: "/images/Certificado_FullStack_MERN.webp",
+    url: "https://verified.sertifier.com/es/verify/56660557762984/",
+  },
+  {
+    name: "Introducción a la Ingeniería de Software",
+    img: "/images/UAM.webp",
+    url: "https://courses.edx.org/certificates/9e839c780889498db42a5d340f59aada",
+  },
+  {
+    name: "Backend Developer",
+    img: "/images/Certificado_Backend.webp",
+    url: "https://verified.sertifier.com/en/verify/99106006683514/",
+  },
+  {
+    name: "Frontend Developer",
+    img: "/images/Certificado_Frontend.webp",
+    url: "https://verified.sertifier.com/es/verify/67005007706277/",
+  },
+  {
+    name: "Javascript Developer",
+    img: "/images/Certificado-Javascript.webp",
+    url: "https://verified.sertifier.com/en/verify/03621406770418/",
+  },
+  {
+    name: "Curso SQL",
+    img: "/images/certificado_SQL.webp",
+    url: "https://tutorial-sql.s3.amazonaws.com/certificates/39285_Clara_Manzano/39285_14112024.png",
+  },
+  {
+    name: "Certificado de excelencia académica",
+    img: "/images/certificado_excelencia.webp",
+    url: "https://verified.sertifier.com/es/verify/22221894159946",
+  },
+];
